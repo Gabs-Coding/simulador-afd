3a932957048f7800813c36eb8b9f7d7988c54212
gabs-coding <gsbiel62@gmail.com>
2024-03-23 17:28:39 -0400

    [afd.h] Declaração das estruturas de dados, funções e procedimentos adicionados para coletar a definção do AFD e manipulá-lo.

--- afd.h
+++ afd.h
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* #Número N de estados do autôomato
    #Estado Inicial
@@ -15,10 +16,44 @@
    #Transições (estado_atual símbolo estado_destino)
    #Cadeia de entrada */
 
+typedef struct transicao {
+  char e_atual; // estado atual da transição;
+  char sim_transicao; // símbolo de transição;
+  char e_destino; // estado de destino da transição;
+} Transicao;
+
+typedef struct lista_duplamente_encadeada ListaDuplamenteEncadeada;
+
+struct lista_duplamente_encadeada {
+  Transicao transicao; // Dado armazenado;
+  ListaDuplamenteEncadeada *ante; // Nó anterior;
+  ListaDuplamenteEncadeada *prox; // Nó posterior;
+};
+
+// Estrutura para armazenar a definição do autômato.
+typedef struct definicao {
+  size_t n; // quantidade de estados do autômato;
+  char ini; // estado inicial;
+  size_t qtd_efinais; // quantidade de estados finais;
+  char *estados_finais; // estados finais do autômato;
+  size_t tam_alfabeto; // número de símbolos do alfabeto;
+  char *simbolos_alfabeto; // símbolos do alfabeto;
+  size_t qtd_transicoes; // número de transições de estados do autômato;
+  char *transicoes; // as transicoes de estado do autômato;
+  char *cadeia_entrada; // cadeia de entrada para testar a sua validade;
+} Definicao;
+
 typedef struct afd {
-  int n; // quantidade de estados do autômato
-  char ini; // estado inicial
-  
+  Definicao *definicao_automato; // Uma cópia da estrutura lida para ser usada pelo autômato;
+  ListaDuplamenteEncadeada *lde_transicoes; // lista duplamente encadeada;
 } AFD;
+
+// Funções e procedimentos para manipular a lista duplamente encadeada;
+void criar(ListaDuplamenteEncadeada *lde);
+bool eh_vazia(ListaDuplamenteEncadeada *lde);
+void inserir(ListaDuplamenteEncadeada *lde, Transicao transicao);
+
+// Funções e procedimentos para manipular o Autômato Finito Determinístico;
+bool tem_transicao(ListaDuplamenteEncadeada *lde, Transicao transicao);
 
 #endif
